cf473c6a7ed264838f57500b75a94e8d64f824f2
StoicNeutron <stoicneutron@Samnangs-MBP.eduroam.gmu.edu>
2023-11-02 15:27:43 -0400

    create project structure

---
+++ src/test/java/com/trackmyworkout/microservices/TmwMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackmyworkout.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmwMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
