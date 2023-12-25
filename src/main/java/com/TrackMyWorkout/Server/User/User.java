89b773f29f3c83f7bb0cf38d67f6a808ed0ef5dc
StoicNeutron <stoicneutron@Samnangs-MBP.fios-router.home>
2023-12-25 14:45:57 -0500

    complete entity and rest controller

--- src/main/java/com/TrackMyWorkout/Server/User/User.java
+++ src/main/java/com/TrackMyWorkout/Server/User/User.java
@@ -1,7 +1,25 @@
+package com.TrackMyWorkout.Server.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity(name ="Users")
 public class User{
 
-    private String userName = null;
-    private String passWord = null;
-    private String email = null;
-    private boolean verified = false;
+    @Id
+    private int id;
+    private String userName;
+    private String passWord;
+    private String email;
+    private boolean verified;
+
+    public User(String userName, String password, String email){
+        this.userName = userName;
+        this.passWord = password;
+        this.email = email;
+    }
+
+    public User() {
+
+    }
 }
--- src/main/java/com/TrackMyWorkout/Server/User/UserController.java
+++ src/main/java/com/TrackMyWorkout/Server/User/UserController.java
@@ -1,3 +1,13 @@
+package com.TrackMyWorkout.Server.User;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController{
-    
+
+    @GetMapping("/new_user")
+    public User createUser(){
+        return new User("sampleUser", "123", "user@email.com");
+    }
 }
