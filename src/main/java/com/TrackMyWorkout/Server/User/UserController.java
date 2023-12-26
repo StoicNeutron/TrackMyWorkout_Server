dc6c4fc423fac76fdaf9518260f7979f153b4c78
StoicNeutron <stoicneutron@Samnangs-MBP.fios-router.home>
2023-12-26 18:01:20 -0500

    progress created repo and service classes

--- src/main/java/com/TrackMyWorkout/Server/User/UserController.java
+++ src/main/java/com/TrackMyWorkout/Server/User/UserController.java
@@ -1,13 +1,27 @@
 package com.TrackMyWorkout.Server.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController{
 
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/new_user")
-    public User createUser(){
-        return new User("sampleUser", "123", "user@email.com");
+    public ResponseEntity<User> createUser(){
+        User user = new User("sampleUser", "123", "user@email.com");
+        this.userService.saveNewUser(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 }
---
+++ src/main/java/com/TrackMyWorkout/Server/User/UserService.java
@@ -0,0 +1,21 @@
+package com.TrackMyWorkout.Server.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public User saveNewUser(User user) {
+        return userRepository.save(user);
+    }
+}
